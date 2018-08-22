bc60f49847359fb6b8c722d899041c9bb3b9c9c5
abasse <andre.basse@spiegel.de>
2018-08-22 16:27:14 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# JSON transformer flogo activity
+This activity allows your flogo application to transform JSON data. The transformation is done with kazaam (https://github.com/qntfy/kazaam).
+
+
+
+## Installation
+
+```bash
+flogo install github.com/abasse/flogojsontransformer
+```
+
+## Schema
+Inputs and Outputs:
+
+```json
+  { "inputs":[
+        {
+          "name": "Input",
+          "type": "string",
+          "required": true
+        },
+        {
+          "name": "Transformation",
+          "type": "string",
+          "required": true
+        }
+      ],
+      "outputs": [
+         {
+           "name": "Result",
+           "type": "string"
+          }
+      ]
+ }
+```
---
+++ activity.go
@@ -0,0 +1,40 @@
+package folgojsontransformer
+
+import (
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+	"github.com/kabukky/kazaam"
+)
+
+// activityLog is the default logger for the exec Activity
+var log = logger.GetLogger("activity-tibco-folgojsontransformer")
+
+type JsontransformerActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new AppActivity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &JsontransformerActivity{metadata: metadata}
+}
+
+// Metadata returns the activity's metadata
+func (a *JsontransformerActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+func (a *JsontransformerActivity) Eval(context activity.Context) (done bool, err error) {
+
+	jsonin, _ := context.GetInput("Input").(string)
+	spec, _ := context.GetInput("Transformation").(string)
+
+	kazaamTransform, _ := kazaam.NewKazaam(spec)
+	kazaamOut, err := kazaamTransform.TransformJSONStringToString(jsonin)
+	if err != nil {
+		log.Info(err)
+	}
+
+	context.SetOutput("result", kazaamOut)
+
+	return true, nil
+}
